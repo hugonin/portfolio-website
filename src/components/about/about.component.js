725b3f7c2fce228918e1d549180bab76b4c4dd11
Hugonin <jeanhugues.doumbe@gmail.com>
2021-11-16 13:57:31 +0100

    updated the styles for the about component

--- src/components/about/about.component.js
+++ src/components/about/about.component.js
@@ -1,7 +1,7 @@
-import * as React from "react";
-import { useStaticQuery, graphql } from "gatsby";
-import { MDXRenderer } from "gatsby-plugin-mdx";
-import { GatsbyImage } from "gatsby-plugin-image";
+import * as React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { MDXRenderer } from "gatsby-plugin-mdx"
+import { GatsbyImage } from "gatsby-plugin-image"
 
 import "./about.styles.css"
 
@@ -27,24 +27,26 @@ const About = () => {
         }
       }
     }
-  `);
+  `)
 
-  const posts = data.allMdx.edges;
+  const posts = data.allMdx.edges
   return (
     <>
       {posts.map(({ node: post }) => {
-        const title = post.frontmatter.title;
-        const subtitle = post.frontmatter.subtitle;
-        const key = post.id;
-        const body = post.body;
+        const title = post.frontmatter.title
+        const subtitle = post.frontmatter.subtitle
+        const key = post.id
+        const body = post.body
         const imageData =
-          post.frontmatter.hero_image.childImageSharp.gatsbyImageData;
-        const imageAlt = post.frontmatter.hero_image_alt;
+          post.frontmatter.hero_image.childImageSharp.gatsbyImageData
+        const imageAlt = post.frontmatter.hero_image_alt
         return (
           <section key={key} className="about-me" id="about" data-nav="About">
-            <h2 className="about-title">{title}</h2>
-            <p className="section-about">{subtitle}</p>
-            <div className="about-body">
+            <h2 className="section_title section_title-about">{title}</h2>
+            <p className="section_subtitle section_subtitle-about">
+              {subtitle}
+            </p>
+            <div>
               <MDXRenderer>{body}</MDXRenderer>
             </div>
 
@@ -54,10 +56,10 @@ const About = () => {
               className="about-image"
             />
           </section>
-        );
+        )
       })}
     </>
-  );
-};
+  )
+}
 
-export default About;
+export default About
